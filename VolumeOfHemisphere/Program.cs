1ab853cbf0db66602778f705c06e69458d5c6989
Joanne <thecodingdiva@gmail.com>
2018-03-20 10:12:24 -0400

    Add project files.

---
+++ VolumeOfHemisphere/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeOfHemisphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const double PI = 3.14159;
+            Console.WriteLine("Enter the radius: ");
+            String r = Console.ReadLine();
+            double radius = Convert.ToDouble(r);
+
+            double area = PI * (radius * radius);
+            double volume = (4 / 3) * radius * area;
+
+            Console.WriteLine("The volume of this hemisphere is: " + volume);
+        }
+    }
+}
